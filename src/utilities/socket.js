61f7b533e87128055c45ec147b3374f5e0e9e44d
Pulkit-Rawat <pulkitrawat.27@gmail.com>
2023-12-06 23:25:29 +0530

    socket base url changed

--- src/utilities/socket.js
+++ src/utilities/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const URL = 'http://localhost:4000';
+const URL = 'https://cbot-server-17dbe4c52b7f.herokuapp.com';
 
 export const socket = io(URL, {
     transports: ['websocket', 'polling', 'flashsocket']
